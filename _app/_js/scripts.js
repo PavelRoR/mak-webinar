dd6730d55c21821745e96a316fc56733b3b1a3e6
pavelp21 <pavelvp2182@yandex.ru>
2020-01-22 18:55:48 +0500

    Main page is typeseted

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,8 +1,25 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend jquery.fancybox.min.js
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-
-/*Конец документа*/
+    $(".video-wrapper-revs img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1"  allowfullscreen></iframe>')
+    });
+    $(function () {
+        $('#video-revs').on('slide.bs.carousel', function () {
+            $('.video-wrapper-revs iframe').each(function () {
+                var l = $(this).parent().attr('data-img');
+                $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+            });
+            $(".video-wrapper-revs img").click(function () {
+                var a = $(this).parent().attr("data-youtube");
+                $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>');
+            });
+        });
+    });
+    /*Конец документа*/
 });
