0cfd61ada034a766441c2e35e1c7887720d605df
pavelp21 <pavelvp2182@yandex.ru>
2020-01-23 19:24:09 +0500

    Main page is adapted

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -2,8 +2,105 @@
 //@prepros-prepend bootstrap.min.js
 //@prepros-prepend jquery.fancybox.min.js
 
-$(document).ready(function () {
+var isIE = false || !!document.documentMode;
 
+if (isIE) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "css/ie.min.css";
+    head.appendChild(link);
+}
+
+$(document).ready(function () {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this),
+            button = $('.button-form', this),
+            switch_title = $('.switch-title'),
+            switch_text = $('.switch-text');
+        // modal_text = $('.modal-text'),
+        // last_container_title = $('.last-container-title'),
+        // currDate = new Date(),
+        // startDate = new Array(
+        //     new Date("January 22 2020 18:30 UTC+3"),
+        //     new Date("January 23 2020 18:30 UTC+3"),
+        //     new Date("January 24 2020 18:30 UTC+3")),
+        // endDate = new Array(
+        //     new Date("January 22 2020 21:00 UTC+3"),
+        //     new Date("January 23 2020 21:00 UTC+3"),
+        //     new Date("January 24 2020 21:00 UTC+3")),
+        // rooms = new Array(
+        //     '',
+        //     '',
+        //     '');
+
+        // function roomSwitch() {
+        //     $('.mail-container .col-md-6').removeClass('col-sm-6');
+        //     $('.mail-container-title').css('marginLeft', '0');
+        //     switch_title.html('А Вы записались на БЕСПЛАТНЫЙ ВЕБИНАР?<br>ЖДЕМ ТОЛЬКО ВАС!').css('lineHeight', '1.2');
+        //     switch_text.removeClass('hide').html('<span class="hurryup">Скорее ЗАХОДИТЕ прямо СЕЙЧАС на вебинар!</span><br>Присоединяйтесь к интенсиву и окунитесь в мир практической философии! Познайте этот мир и себя в нём!');
+        //     button.text('Зайти!');
+        // }
+        // if ((currDate > startDate[0] && currDate < endDate[0]) || (currDate > startDate[1] && currDate < endDate[1]) || (currDate > startDate[2] && currDate < endDate[2])) {
+        //     roomSwitch();
+        // }
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this),
+                button = $('.button-form', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
+                // if (currDate > startDate[0] && currDate < endDate[0]) {
+                //     window.open(rooms[0]);
+                // }
+                // if (currDate > startDate[1] && currDate < endDate[1]) {
+                //     window.open(rooms[1]);
+                // }
+                // if (currDate > startDate[2] && currDate < endDate[2]) {
+                //     window.open(rooms[2]);
+                // }
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
 
     $(".video-wrapper-revs img").click(function () {
         var a = $(this).parent().attr("data-youtube");
